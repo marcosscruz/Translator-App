3010745cb77626011bc7fab09b8ed51446f8c6a8
Marcos <125505999+marcosscruz@users.noreply.github.com>
2023-03-27 11:14:24 -0300

    diversas mudanças

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-const fromText = document.querySelector(".from-text")
-const toText = document.querySelector(".to-text")
-const exchangeIcon = document.querySelector(".exchange")
-const selectTag = document.querySelectorAll(".select")
-const icons = document.querySelectorAll(".row i")
-const translateBtn = document.querySelector(".button")
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelectorAll(".select");
+const icons = document.querySelectorAll(".row i");
+const translateBtn = document.querySelector("button");
 
-
-selectTag.forEach((tag, id) => {
-    for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "BR" ? "selected" : "" : country_code == "US" ? "selected" : "";
-        let option = `<option ${selected} value="${country_code}">${country_code[country_code]}</option`;
-        tag.insertAdjacentHTML("beforeend", option);
-    }
-});
-
-exchangeIcon.addEventListener("click", ()=>{
-    let tempText=fromText.value,
-        tempLang = selectTag[0].value;
-    fromText.value=toText.value;
-    toText.value=tempText;
-    selectTag[0].value=selectTag[1].value;
-    selectTag[1].value=tempLang;
-})
